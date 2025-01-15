8905cbcab31a2f59ef4a49a73e17b19178979f1f
TaHaOuahbi <tahaouahbiwork@gmail.com>
2025-01-15 18:08:07 +0100

    -Add Little animation into action bar (Gif Renault Logo).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,8 @@ dependencies {
     implementation(libs.cardview)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation (libs.gifview)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
